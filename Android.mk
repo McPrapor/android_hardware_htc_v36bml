5d2741974fca2aed8428fead59a22c7028f08ffa
McPrapor <max.terleckiy@gmail.com>
2019-02-14 11:39:39 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@
 # by: daniel_hk (https://github.com/danielhk)
 #
 LOCAL_PATH := $(my-dir)
-ifeq ($(TARGET_DEVICE),note3)
+ifeq ($(TARGET_DEVICE),htc_v36bml_dugl)
 include $(call first-makefiles-under,$(LOCAL_PATH))
 include hardware/ril/librilutils/Android.mk
 endif
